cbcf17206e1a4fdde2e81b91355c4e297299db31
alexfornuto <alex@fornuto.com>
2014-08-05 13:21:42 -0400

    first set up scripts

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo -e "Updating, upgrading, installing packages... \n"
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install git ruby rake 
+
+echo -e "Configuring git variables... \n"
+git config --global color.ui auto
+git config --global user.name alexfornuto
+git config --global user.email alex@fornuto.com
+
+echo -e "Making .scripts folder, if not already there... \n"
+mkdir -p ~/.scripts
+
+echo -e "Adding user.sh to .scripts folder... \n"
+cp scripts/user.sh ~/.scripts/user.sh
+
+echo -e "Copying Screen config file into ~ ... \n"
+cp configs/.screenrc ~/.screenrc
+
+echo -e "Adding the list of public keys from this repo into the authorized_keys file for this user... \n"
+cat configs/authorized_keys >> ~/.ssh/authorized_keys
+
+echo -e "Done! \n"
